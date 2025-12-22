8a13110e8135bbc206fcb789c4595668de0b9b19
Mbocpe <bocminhkhoi@gmail.com>
2025-12-22 10:25:29 +1000

    set up search bar and log results

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '38992095b1msh0cb65111b59b0dap1feeb8jsn7359811a4ccf',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
