00391b6b35a9570dbf47a6cdccce58a9c1a36e48
Mbocpe <bocminhkhoi@gmail.com>
2025-12-25 15:39:20 +1000

    Added UI weather components

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,10 +4,34 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
         <div className="top">
-            <p className="city">Brisbane</p>
-            <p className="weather-desc">Hot as hell</p>
+            <div>
+                <p className="city">Brisbane</p>
+                <p className="weather-desc">Hot as hell</p>
+            </div>
+            <img alt="weather" className="weather-icon" src="icons/01d.png"/>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+
+        <div className="bottom">
+            <p className="temp">34°C</p>
+            <div className="details">
+                <div className="parameter-row">
+                    <span className="parameter-label">Feels like</span>
+                    <span className="parameter-value">38°C</span>
+                </div>
+                <div className="parameter-row">
+                    <span className="parameter-label">Wind</span>
+                    <span className="parameter-value">6 km/h</span>
+                </div>
+                <div className="parameter-row">
+                    <span className="parameter-label">Humidity</span>
+                    <span className="parameter-value">67%</span>
+                </div>
+                <div className="parameter-row">
+                    <span className="parameter-label">Pressure</span>
+                    <span className="parameter-value">15 hPa</span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
