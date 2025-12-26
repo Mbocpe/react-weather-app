4776d6637542e5ec175451214f848acba5743e43
Mbocpe <bocminhkhoi@gmail.com>
2025-12-26 13:31:28 +1000

    Set up API calls from OpenWeather

--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweatherapp.org/data/3.0';
+export const WEATHER_API_KEY = '151540a463f5fd78f805be71e38a2a87';
